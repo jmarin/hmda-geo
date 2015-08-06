8ef94fee8da887be7879079fa4b75618263cea11
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-08-06 10:06:07 -0400

    Update version

--- project/Version.scala
+++ project/Version.scala
@@ -4,9 +4,9 @@ object Version {
   val scalaTest       = "2.2.1"
   val logback         = "1.1.2"
   val scalaLogging    = "3.1.0"
-  val slick           = "3.0.0"
+  val slick           = "3.0.1"
   val slickPG         = "0.9.1"
-  val hikariCP        = "2.3.5"
+  val hikariCP        = "2.3.7"
   val jts             = "1.13"
   val scale           = "0.0.2"
 }
